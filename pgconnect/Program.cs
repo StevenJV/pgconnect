6ff349c732f461a78fdec391569a3f11d9fe5c49
StevenJV <StevenVore@tnwinc.com>
2016-06-23 15:57:15 -0400

    using CommandRunner and db.Execute, moving connection to App.config

--- pgconnect/Program.cs
+++ pgconnect/Program.cs
@@ -1,52 +1,25 @@
 ﻿using System;
 using Npgsql;
+using System.Configuration;
+using System.Collections;
 
 namespace pgconnect
 {
   class Program
   {
     static void Main(string[] args) {
-      var userId = "postgres";
-      var password = "none";
-      userId = GetUsername(userId);
-      password = GetPassword();
-      var connectionString = BuildConnectionString(userId, password, "localhost", "dvdrental");
-      using (var conn = new NpgsqlConnection(connectionString)) {
-        conn.Open();
-        var cmd = new NpgsqlCommand("select * from film", conn);
-        var rdr = cmd.ExecuteReader();
-        while (rdr.Read()) {
-          Console.WriteLine(rdr["title"]);
-        }
-        cmd.Dispose();
-        conn.Dispose();
+      var db = new CommandRunner("dvds");
+      var films = db.Execute<Film>("select * from film");
+      foreach (var film in films)
+      {
+        Console.WriteLine(film.Title);
       }
       Console.Read();
-
     }
+  }
 
-    public class Film
-    {
-      public string Title { get; set; }
-    }
-
-    private static string BuildConnectionString(string userId, string password, string server, string database) {
-      var connectionString = "server=" + server + ";user id =" + userId + ";password=" + password + ";database=" +
-                                database;
-      return connectionString;
-    }
-
-    private static string GetPassword() {
-      Console.Write("password: ");
-      var password = Console.ReadLine();
-      return password;
-    }
-
-    private static string GetUsername(string defaultUserId) {
-      Console.Write("username (enter=" + defaultUserId + "): ");
-      var userIdInput = Console.ReadLine();
-      var userId = userIdInput != "" ? userIdInput : defaultUserId;
-      return userId;
-    }
+  public class Film
+  {
+    public string Title { get; set; }
   }
 }
