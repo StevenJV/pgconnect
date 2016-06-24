f3d93db430f7045562106fe6af109fe259509337
StevenJV <StevenVore@tnwinc.com>
2016-06-24 15:07:48 -0400

    sample of calling a stored procedure

--- pgconnect/Program.cs
+++ pgconnect/Program.cs
@@ -9,10 +9,16 @@ namespace pgconnect
   {
     static void Main(string[] args) {
       var db = new CommandRunner("dvds");
-      var films = db.Execute<Film>("select * from film");
-      foreach (var film in films)
+      //var films = db.Execute<Film>("select * from film");
+      //foreach (var film in films)
+      //{
+      //  Console.WriteLine(film.Title);
+      //}
+      Console.WriteLine("---");
+      var actors = db.Execute<Actor>("select * from actorswithlastname('Pitt')");
+      foreach (var actor in actors)
       {
-        Console.WriteLine(film.Title);
+        Console.WriteLine("{0} {1} {2}",actor.actor_id.ToString(),actor.first_name,actor.last_name);
       }
       Console.Read();
     }
@@ -21,5 +27,12 @@ namespace pgconnect
   public class Film
   {
     public string Title { get; set; }
+  }
+
+  public class Actor
+  {
+    public int actor_id {get;set;}
+    public string first_name { get; set; }
+    public string last_name { get; set; }
   }
 }
---
+++ pgconnect/sql/fnActorsWithLastName.sql
@@ -0,0 +1,20 @@
+﻿create or replace function actorswithlastname(
+ lastname varchar(45)
+)
+returns TABLE ( 
+  actor_id integer ,
+  first_name character varying(45),
+  last_name character varying(45)  
+ ) as
+ $$
+ DECLARE
+	success BOOLEAN;
+ BEGIN
+  if (lastname is null) then 
+	select 'false' into success;
+  else
+	return query
+	select actor.actor_id, actor.first_name, actor.last_name from actor where actor.last_name = lastname;
+  end if;
+END;  
+ $$ LANGUAGE PLPGSQL  
